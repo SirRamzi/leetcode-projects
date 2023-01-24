eb876ed32876834cacba1ac1b4a43091f2bca594
SirRamzi <aleks.aleksp@gmail.com>
2023-01-24 23:35:38 +0400

    Add NumberOfSteps1372 class including task 1372 solution

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println(NumberOfSteps1372.numberOfSteps(14));;
     }
 }
